4e72517641e5482a43ea7f4f68a95b27431b0364
Bruno Prestes <brunoasprestes@gmail.com>
2022-11-01 16:01:33 -0300

    Push das repostas selecionadas

--- js/result.js
+++
@@ -1,9 +0,0 @@
-const r = document.getElementById("resultado");
-
-if(game.getResult()) {
-    r.innerHTML = "Aprovado"
-    console.log(game.replys);
-} else {
-    r.innerHTML = "Reprovado"
-    console.log(game.replys);
-}
